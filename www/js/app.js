159d2397b0106eaea3a98390d6bca6d3afecdf8d
enamul95 <ehaque95@gmail.com>
2015-02-03 11:20:54 -0800

    a

--- www/js/app.js
+++ www/js/app.js
@@ -3,17 +3,81 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic'])
+ 	var db = null;
+ 
+angular.module('starter', ['ionic', 'ngCordova'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-    // for form inputs)
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
-  });
+
+.config(function($stateProvider, $urlRouterProvider) {
+    $stateProvider
+        .state('signin', {
+          url: "/signin",
+          templateUrl: "signin.html",
+          controller: 'SignInCtrl'
+        })
+
+    
+        .state('welcome', {
+          url: "/welcome",
+          templateUrl: "welcome.html",
+		   controller: 'welcomeCtrl'
+		  
+        })
+		
+	
+    
+    $urlRouterProvider.otherwise("/signin");
 })
+
+.controller('SignInCtrl', function($scope, $state, $http,$cordovaSQLite,$location) {
+
+  $scope.insert = function() {
+  alert("Call Method");
+        var query = "INSERT INTO people (firstname, lastname) VALUES (?,?)";
+        $cordovaSQLite.execute(db, query, ['Enamul', 'Haque']).then(function(res) {
+		 alert("Insert Method");
+            console.log("INSERT ID -> " + res.insertId);
+        }, function (err) {
+		 alert("Failure");
+            console.error(err);
+        });
+    }
+ 
+    $scope.select = function() {
+	 alert("Call Method");
+        var query = "SELECT firstname, lastname FROM people ";
+        $cordovaSQLite.execute(db, query).then(function(res) {
+            if(res.rows.length > 0) {
+			 alert("Select Method");
+                console.log("SELECTED -> " + res.rows.item(0).firstname + " " + res.rows.item(0).lastname);
+            } else {
+                console.log("No results found");
+            }
+        }, function (err) {
+            console.error(err);
+			 alert("Error Method");
+        });
+    }
+})
+.controller('welcomeCtrl', function($scope, $state, $http,$ionicLoading) {
+
+})
+
+   .run(function($ionicPlatform, $cordovaSQLite) {
+  // alert("dddd");
+        $ionicPlatform.ready(function() {
+            if(window.cordova && window.cordova.plugins.Keyboard) {
+                cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+            }
+            if(window.StatusBar) {
+                StatusBar.styleDefault();
+            }
+			//SQLite database
+			db = $cordovaSQLite.openDB("my.db", 1);
+		
+			
+			  //db.transaction((tx) {})
+            //db = $cordovaSQLite.openDB({ name: "my.db" });
+           $cordovaSQLite.execute(db, "CREATE TABLE IF NOT EXISTS people (id integer primary key, firstname text, lastname text)");
+        });
+    });
